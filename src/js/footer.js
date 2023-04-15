957063acf2b63d4be8d6d144787d59d2b4fddb14
toddbrentlinger <toddbrentlinger@gmail.com>
2023-04-14 17:58:45 -0700

    Add footer component and styling
    Adds footer component that displays year range if current year is past
    year project began.
    Adds meyer reset stylesheet.
    Starts styling of page.

---
+++ src/js/footer.js
@@ -0,0 +1,22 @@
+import { createElement } from "./utilities.js";
+
+export default function Footer(copyrightYear) {
+    const render = function() {
+        const footer = createElement('footer', {'class': 'white-text-shadow'});
+        const currYear = new Date().getFullYear();
+
+        // Paragraph element as child of footer
+        let tempElement = footer.appendChild(document.createElement('p'));
+
+        // Small element as child of paragraph
+        tempElement = tempElement.appendChild(createElement('small', {},
+            'Source Code © ',
+            createElement('time', {id: 'copyright-year'}, currYear > copyrightYear ? `${copyrightYear}-${currYear}` : copyrightYear),
+            ' Todd Brentlinger, Santa Cruz, CA, USA. All Rights Reserved.'
+        ));
+
+        return footer;
+    };
+
+    return {render,};
+}
