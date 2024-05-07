38329ad2888f21805c2c2cd4068fa461854aed51
toddbrentlinger <toddbrentlinger@gmail.com>
2024-05-07 12:28:51 -0700

    Improve component that displays current progress of cut list calculation
    Replaces number argument in update function, that is treated as a progress percent,
    with string argument of any progress message.
    Adds comments and docstrings, including using typedef to define a custom object for
    the factory function return type.
    Improves formatting according to Airbnb style guide.

--- src/js/components/progressBarComponent.js
+++ src/js/components/progressBarComponent.js
@@ -1,13 +1,34 @@
 import { createElement } from "../utilities";
 
-export default function ProgressBarComponent() {
-    let containerElement;
-    let valueElement;
+/**
+ * @typedef {Object} ProgressBarComponent
+ * @property {Function} hide - Hides container HTMLElement from DOM
+ * @property {Function} unhide - Displays container HTMLElement in DOM
+ * @property {Function} update - Updates progress message on value HTMLElement
+ * @property {Function} render - Returns HTMLElement of progress bar component
+ */
 
+/**
+ * Factory function to create an instance of a progress bar component.
+ * @returns {ProgressBarComponent}
+ */
+export default function ProgressBarComponent() {
+    // Reference to container element of entire progress bar component
+    let containerElement;
+
+    // Reference to value element that displays current progress message
+    let valueElement;
+    
+    /**
+     * Hides container HTMLElement from DOM. 
+     */
     const hide = function() {
         containerElement.classList.add('hide');
     };
 
+    /**
+     * Displays container HTMLElement in DOM.
+     */
     const unhide = function() {
         containerElement.classList.remove('hide');
         containerElement.scrollIntoView({
@@ -15,18 +36,31 @@ export default function ProgressBarComponent() {
         });
     };
 
-    const update = function(num) {
-        valueElement.textContent = `Progress: ${num}%`;
+    /**
+     * Updates progress message on value HTMLElement.
+     * @param {string} str 
+     */
+    const update = function(str = '') {
+        valueElement.textContent = str;
     };
 
+    /**
+     * Returns HTMLElement of progress bar component.
+     * @returns {HTMLElement}
+     */
     const render = function() {
-        containerElement = createElement('section', {'class': 'progress-container hide'});
-
-        valueElement = containerElement.appendChild(
-            createElement('div', {'class': 'progress-value'})
+        // Create container element
+        containerElement = createElement(
+            'section', 
+            { 'class': 'progress-container hide', }
         );
-        update(0);
 
+        // Create value element as child of container to hold progress message
+        valueElement = containerElement.appendChild(
+            createElement('div', { 'class': 'progress-value', })
+        );
+
+        // Add animated spinning image to indicate progress is currently running
         containerElement.appendChild(
             createElement('div', {'class': 'progress-icon-container'}, 
                 createElement('span', {'class': 'fa-solid fa-spinner'})
@@ -36,5 +70,5 @@ export default function ProgressBarComponent() {
         return containerElement;
     };
 
-    return {hide, unhide, update, render,}
+    return { hide, unhide, update, render, };
 }
